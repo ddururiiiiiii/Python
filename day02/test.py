fe0f68f503cf6b1a31a62083f812905182b031d5
ddururiiiiiii <78954385+ddururiiiiiii@users.noreply.github.com>
2021-05-03 19:13:00 +0900

    Delete test.py

--- day02/test.py
+++
@@ -1,9 +0,0 @@
-#04 첫숫자~끝숫자 짝수의 합은?
-num1 = int(input("첫 숫자를 입력하세요."))
-num2 = int(input("끝 숫자를 입력하세요."))
-
-sum4 = 0
-for i in range(num1, num2+1) : 
-    if(i % 2) == 0 :
-        sum4 += i
-print("첫숫자~끝숫자 짝수의 합은 ? ", sum4)
